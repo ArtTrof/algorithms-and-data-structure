c00d89d4c57bdfcba377d1705f8991b9282a9e4f
Artem Trofymenko <randallsng@gmail.com>
2023-04-12 13:29:59 +0200

    greedy finding max possible nubmer

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -10,6 +10,7 @@ public class Fibonacci {
         System.out.println(fibNaive(n, mem));
     }
 
+    //O(2^N) without memoization
     private static long fibNaive(int n, long[] mem) {
         if (mem[n] != -1)
             return mem[n];
@@ -19,14 +20,20 @@ public class Fibonacci {
         mem[n] = result;
         return result;
     }
-
+    //O(N)
     private static long fibEffective(int n) {
+        //O(N)
         long arr[] = new long[n + 1];
-        arr[0] = 0;
-        arr[1] = 1;
+        arr[0] = 0;//O(1)
+        arr[1] = 1;//O(1)
+        //n=2=>1
+        //n=3=>2
+        //n=3=>2
+
+        //O(n+n)=>O(2n)=>O(N)
         for (int i = 2; i <= n; i++) {
             arr[i] = arr[i - 1] + arr[i - 2];
         }
-        return arr[n];
-    }
+        return arr[n];//O(1)
+    }//O(N)+O(1)+O(1)+O(N)+O(N)+O(1)=O(2n+3)=O(N)
 }
