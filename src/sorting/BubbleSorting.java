2b0f2876c1e350d6f29b45c3d6551ce35c09fba4
Artem Trofymenko <randallsng@gmail.com>
2023-04-14 15:15:48 +0200

    bubble sorting

---
+++ src/sorting/BubbleSorting.java
@@ -0,0 +1,23 @@
+package sorting;
+
+public class BubbleSorting {
+    public static void main(String[] args) {
+        int[]arr = new int[]{2,4,14,2,5};
+        bubbleSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void bubbleSort(int[] sortArr) {
+        for (int i = 0; i < sortArr.length - 1; i++) {
+            for (int j = 0; j < sortArr.length - i - 1; j++) {
+                if (sortArr[j + 1] < sortArr[j]) {
+                    int swap = sortArr[j];
+                    sortArr[j] = sortArr[j + 1];
+                    sortArr[j + 1] = swap;
+                }
+            }
+        }
+    }
+}
